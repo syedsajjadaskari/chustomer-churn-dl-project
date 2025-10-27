7ee76eba0e1a501c151e0b116ed15ce95567a030
syedsajjadaskari <syedsajjad62@gmail.com>
2025-10-27 07:56:34 +0530

    import of python packesges

---
+++ models/train.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.callbacks import EarlyStopping
+from tenflow.keras.models import load_model
+
