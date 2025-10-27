e9249ddeeed85862050988cc145ff2c003cab020
syedsajjadaskari <syedsajjad62@gmail.com>
2025-10-27 08:19:25 +0530

    transform of the data

--- models/train.py
+++ models/train.py
@@ -1,9 +1,22 @@
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, LabelEncoder
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.callbacks import EarlyStopping
 from tenflow.keras.models import load_model
+
+df  = pd.read_csv('data/customer_churn.csv')
+
+X = df.drop(['CustomerId','Surname','Exited'], axis=1)
+y = df['Exited']
+
+X['Geography'] = LabelEncoder().fit_transform(X['Geography'])
+X['Gender'] = LabelEncoder().fit_transform(X['Gender'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+#tranform of the data 
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
 
