7d6ce05df6990b634b2de5cbae3a56f5dbebb357
syedsajjadaskari <syedsajjad62@gmail.com>
2025-10-28 07:13:24 +0530

    complete code of train.py

--- models/train.py
+++ models/train.py
@@ -15,8 +15,27 @@ y = df['Exited']
 X['Geography'] = LabelEncoder().fit_transform(X['Geography'])
 X['Gender'] = LabelEncoder().fit_transform(X['Gender'])
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 #tranform of the data 
 scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
+X_Scaled = scaler.fit_transform(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = Sequential([
+    Dense(16, activation ='relu', input_shape=(X_train.shape[1],)),
+    Dense(8, activation='relu'),
+    Dense(1, activation='sigmoid')
+
+])
+
+model.compile(optimizer='adam', loss= 'binary_crossentropy', metrics=['accuracy'])
+
+model.fit(X_train, y_train, epochs = 30,batch_size=13, validation_split = 0.2, verbose = 1)
+
+
+
+model.save('models/churn_model.h5')
+pd.to_pickle(scaler,'.model/scler.pkl')
+
+
 
